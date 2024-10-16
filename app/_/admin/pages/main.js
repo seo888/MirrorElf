d38aa135c7bfe19f859669f278e9cb55522639bf
Seo <53137698+seo888@users.noreply.github.com>
2024-10-16 18:00:36 +0000

    update

--- app/_/admin/pages/main.js
+++ app/_/admin/pages/main.js
@@ -1,15 +1,20 @@
-(function() {
+(function () {
 	const response = {
 		data: {
 			type: "page",
 			title: "仪表盘",
-			body: {
-				"type": "log",
-				"height": 300,
-				"rowHeight": 22,
-				// 'maxLength':200,
-				"source": "/_api_/logs"
-			  }
+			body: [{
+				"type": "panel",
+				"title": "网站实时日志",
+				"body": {
+					"type": "log",
+					"height": 300,
+					// "rowHeight": 22,
+					'maxLength': 200,
+					"source": "/_api_/logs",
+					"operation": ['stop', 'restart', 'clear', 'showLineNumber', 'filter']
+				}
+			},]
 		},
 		status: 0
 	}
--- app/lock/__init__.py
+++ app/lock/__init__.py
@@ -1,2 +1,2 @@
-# Pyarmor 8.5.12 (basic), 004374, 2024-10-16T17:25:14.859962
+# Pyarmor 8.5.12 (basic), 004374, 2024-10-16T18:00:31.433060
 from .pyarmor_runtime import __pyarmor__
