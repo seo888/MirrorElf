8e9b0d6ff1a146d454b1cca3b3a8ca774c213789
Seo <53137698+seo888@users.noreply.github.com>
2024-10-16 10:30:35 +0000

    update

---
+++ app/ips.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# 定义输出文件
+IPS_TXT="./config/IPS.txt"
+
+# 获取所有 IP 地址并排除 127.0.0.1 和局域网 IP 地址
+ips=$(ip addr | grep 'inet ' | awk '{print $2}' | cut -d/ -f1 | \
+    grep -v '^127.0.0.1$' | \
+    grep -vE '^10\.|^172\.(1[6-9]|2[0-9]|3[0-1])\.|^192\.168\.')
+
+# 将 IP 地址写入文件
+echo "$ips" > "$IPS_TXT"
---
+++ app/lock/__init__.py
@@ -0,0 +1,2 @@
+# Pyarmor 8.5.12 (basic), 004374, 2024-10-16T10:29:13.745184
+from .pyarmor_runtime import __pyarmor__
