43bcb2a9b1deb60bf5e1034fae3eb60e206b44b6
Seo <53137698+seo888@users.noreply.github.com>
2024-10-26 07:38:13 +0000

    update

--- app/lock/__init__.py
+++ app/lock/__init__.py
@@ -1,2 +1,2 @@
-# Pyarmor 8.5.12 (basic), 004374, 2024-10-26T06:59:48.596601
+# Pyarmor 8.5.12 (basic), 004374, 2024-10-26T07:37:06.563570
 from .pyarmor_runtime import __pyarmor__
--- update.sh
+++ update.sh
@@ -10,7 +10,7 @@ mkdir -p ./data/garnet_data
 
 # 执行更新脚本
 docker exec mirror_elf python update.py
-rm -rf /www/Mirror-Elf/app/core
+rm -rf /www/Mirror-Elf/app/core*
 
 # 从 repo 目录复制最新的 update.sh 文件，覆盖现有文件
 cp /www/Mirror-Elf/app/repo/update.sh /www/Mirror-Elf/update.sh
@@ -26,10 +26,9 @@ docker rmi mirror-elf-mirror_elf || true  # 如果镜像不存在则忽略错误
 docker rmi $(docker images -q mirror-elf-celery_worker*) || true  # 同样忽略错误
 
 # 删除 rabbitmq_data 卷
-# docker volume rm mirror-elf_rabbitmq_data || true  # 如果卷不存在则忽略错误
+docker volume rm mirror-elf_rabbitmq_data || true  # 如果卷不存在则忽略错误
 
 docker system prune -a -f
-
 
 # 重新启动 Docker Compose
 docker compose up -d --remove-orphans
