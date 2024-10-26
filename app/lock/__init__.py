7c79a861a361684df72013d5e9d9be9ada31a1d1
Seo <53137698+seo888@users.noreply.github.com>
2024-10-26 06:59:55 +0000

    update

--- app/lock/__init__.py
+++ app/lock/__init__.py
@@ -1,2 +1,2 @@
-# Pyarmor 8.5.12 (basic), 004374, 2024-10-26T06:40:18.019566
+# Pyarmor 8.5.12 (basic), 004374, 2024-10-26T06:59:48.596601
 from .pyarmor_runtime import __pyarmor__
--- update.sh
+++ update.sh
@@ -10,24 +10,26 @@ mkdir -p ./data/garnet_data
 
 # 执行更新脚本
 docker exec mirror_elf python update.py
+rm -rf /www/Mirror-Elf/app/core
 
 # 从 repo 目录复制最新的 update.sh 文件，覆盖现有文件
 cp /www/Mirror-Elf/app/repo/update.sh /www/Mirror-Elf/update.sh
 cp /www/Mirror-Elf/app/repo/docker-compose.yml /www/Mirror-Elf/docker-compose.yml
 
 # 停止 Docker Compose
-# docker compose down
-docker compose down --rmi all --volumes
+docker compose down
+# docker compose down --rmi all --volumes
+
+# 删除指定镜像
+docker rmi rabbitmq:3-management  || true  # 如果镜像不存在则忽略错误
+docker rmi mirror-elf-mirror_elf || true  # 如果镜像不存在则忽略错误
+docker rmi $(docker images -q mirror-elf-celery_worker*) || true  # 同样忽略错误
 
 # 删除 rabbitmq_data 卷
-docker volume rm mirror-elf_rabbitmq_data || true  # 如果卷不存在则忽略错误
+# docker volume rm mirror-elf_rabbitmq_data || true  # 如果卷不存在则忽略错误
 
 docker system prune -a -f
-# 删除指定镜像
-# docker system prune -a --volumes
-# docker rmi rabbitmq:3-management  || true  # 如果镜像不存在则忽略错误
-# docker rmi mirror-elf-mirror_elf || true  # 如果镜像不存在则忽略错误
-# docker rmi $(docker images -q mirror-elf-celery_worker*) || true  # 同样忽略错误
+
 
 # 重新启动 Docker Compose
 docker compose up -d --remove-orphans
