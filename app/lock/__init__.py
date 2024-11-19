e0f3af413a4dbf6052b86bf505199ba23fe1bf64
Seo <53137698+seo888@users.noreply.github.com>
2024-11-19 08:41:24 +0000

    update

--- app/lock/__init__.py
+++ app/lock/__init__.py
@@ -1,2 +1,2 @@
-# Pyarmor 8.5.12 (basic), 004374, 2024-11-11T07:04:47.885093
+# Pyarmor 8.5.12 (basic), 004374, 2024-11-19T08:41:07.072284
 from .pyarmor_runtime import __pyarmor__
--- task/lock/__init__.py
+++ task/lock/__init__.py
@@ -1,2 +1,2 @@
-# Pyarmor 8.5.12 (basic), 004374, 2024-11-11T07:04:49.049113
+# Pyarmor 8.5.12 (basic), 004374, 2024-11-19T08:41:07.966777
 from .pyarmor_runtime import __pyarmor__
